6ca41549f5472e022c5c75cc942694c3b402381d
Albert Garcia-Garcia <agarcia@dtic.ua.es>
2018-11-05 18:02:02 +0100

    add WIP version of PyTorch Geometric data loader #1

--- train.py
+++ train.py
@@ -2,10 +2,13 @@ import logging
 import sys
 
 import torch
+import torch.utils.data.dataloader
 
 from torch_geometric.data import Data
+from torch_geometric.data import DataLoader
 
 import loader.biotacsp_loader
+import dataset.biotacsp
 import transforms.tograph
 import utils.plotcontour
 import utils.plotgraph
@@ -16,26 +19,26 @@ def train():
 
     CSV_FILE = "biotac-palmdown-grasps.csv"
 
-    biotacsp_dataset_ = loader.biotacsp_loader.BioTacSpDataset(csvFile=CSV_FILE)
+    transform_tograph_ = transforms.tograph.ToGraph()
+
+    biotacsp_dataset_ = dataset.biotacsp.BioTacSp(root='~/Workspace/3dpl/tactile-gcn/data/biotacsp')
+    biotacsp_loader_ = DataLoader(biotacsp_dataset_, batch_size=4, shuffle=True, num_workers=4)
 
     # Transformations
-    transform_tograph_ = transforms.tograph.ToGraph(biotacsp_dataset_.m_taxels_x, biotacsp_dataset_.m_taxels_y)
 
     log.info(biotacsp_dataset_)
 
-    for i in range(len(biotacsp_dataset_)):
-
-        sample_ = biotacsp_dataset_[i]
-        log.info(sample_)
+    for batch in biotacsp_loader_:
+        
+        batch
+        #sample_ = biotacsp_dataset_[i]
+        #log.info(sample_)
         #utils.plotcontour.plot_contour(sample_, biotacsp_dataset_.m_taxels_x, biotacsp_dataset_.m_taxels_y)
 
-        graph_sample_ = transform_tograph_(sample_)
+        #graph_sample_ = transform_tograph_(sample_)
 
-        utils.plotgraph.plot_contourgraph(graph_sample_)
+        #utils.plotgraph.plot_contourgraph(graph_sample_)
         #utils.plotgraph.plot_graph(graph_sample_)
-
-
-
 
 if __name__ == "__main__":
 
--- transforms/tograph.py
+++ transforms/tograph.py
@@ -8,17 +8,16 @@ log = logging.getLogger(__name__)
 
 class ToGraph(object):
 
-    def __init__(self, xTaxels, yTaxels):
-        self.m_taxels_x = xTaxels
-        self.m_taxels_y = yTaxels
+    def __init__(self):
 
+        self.m_taxels_x = [-3.0, -2.0, -4.0, -2.5, -1.5, -4.0, -2.5, -0.5, -2.0, -2.5, 3.0, 2.0, 4.0, 2.5, 1.5, 4.0, 2.5, 0.5, 2.0, 2.5, 0.0, -1.0, 1.0, 0.0]
+        self.m_taxels_y = [5.0, 4.0, 1.0, 0.0, -1.0, -2.0, -3.0, -4.0, -5.0, -6.0, 5.0, 4.0, 1.0, 0.0, -1.0, -2.0, -3.0, -4.0, -5.0, -6.0, 3.0, 2.0, 2.0, 0.0]
         self.m_edge_origins =   [0, 1, 1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 10, 11, 11, 12, 13, 13, 13, 14, 14, 14, 15, 16, 16, 16, 16, 17, 17, 17, 17, 18, 18, 18, 19, 20, 20, 20, 20, 21, 21, 21, 21, 22, 22, 22, 22, 23, 23, 23, 23, 23, 23]
         self.m_edge_ends =      [1, 0, 20, 3, 2, 4, 21, 23, 3, 6, 7, 23, 6, 5, 4, 7, 8, 4, 6, 8, 17, 6, 7, 9, 8, 11, 10, 20, 13, 12, 23, 14, 13, 16, 17, 16, 15, 14, 17, 18, 14, 16, 18, 7, 17, 16, 19, 18, 1, 11, 21, 22, 3, 20, 22, 23, 13, 20, 21, 23, 21, 22, 3, 13, 4, 14]
 
     def __call__(self, sample):
 
         # Index finger
-        log.debug(sample['data_index'])
         index_graph_x_ = torch.tensor(sample['data_index'], dtype=torch.float)
         index_graph_edge_index_ = torch.tensor([self.m_edge_origins, self.m_edge_ends], dtype=torch.long)
         index_graph_pos_ = torch.tensor(np.vstack((self.m_taxels_x, self.m_taxels_y)), dtype=torch.float)
