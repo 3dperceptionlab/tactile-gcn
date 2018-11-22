00187cc4453a5d3d72d4f2531127fb2cd32c8c53
Albert Garcia <agarcia@dtic.ua.es>
2018-11-22 19:42:25 +0100

    add testing no pooling network which achieves 84% acc. #10
    We have added a temporary network under test that does not use
    pooling to generate the output predictions. Instead it uses fully
    connected layers with a flattened input coming from the previous
    convolutional layers. Then the output of such connected layers is
    reshaped to the needed output shape. This configuration achieves
    the milestone of 84% accuracy.

--- network/gcn_network.py
+++ network/gcn_network.py
@@ -1,3 +1,4 @@
+import logging
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,10 +11,41 @@ from torch_geometric.utils import normalized_cut
 from torch_geometric.nn import (SplineConv, graclus, max_pool, max_pool_x,
 global_mean_pool)
 
+log = logging.getLogger(__name__)
+
 def normalized_cut_2d(edge_index, pos):
     row, col = edge_index
     edge_attr = torch.norm(pos[row] - pos[col], p=2, dim=1)
     return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
+
+class GCN_test(torch.nn.Module):
+
+    def __init__(self, numFeatures, numClasses):
+
+        super().__init__()
+
+        self.conv1 = GCNConv(numFeatures, 8)
+        self.conv2 = GCNConv(8, 8)
+        self.conv3 = GCNConv(8, 16)
+        self.conv4 = GCNConv(16, 16)
+        self.fc1 = torch.nn.Linear(384, 128)
+        self.fc2 = torch.nn.Linear(128, numClasses * 1)
+
+    def forward(self, data):
+        #log.info("NETWOROOOOOORK")
+        data.x = F.relu(self.conv1(data.x, data.edge_index))
+        data.x = F.relu(self.conv2(data.x, data.edge_index))
+        data.x = F.relu(self.conv3(data.x, data.edge_index))
+        data.x = F.relu(self.conv4(data.x, data.edge_index))
+        #data.x = F.dropout(data.x, training=self.training)
+
+        log.debug(data.x.view(-1).size())
+        data.x = self.fc1(data.x.view(-1))
+        data.x = self.fc2(data.x)
+        log.debug(data.x.size())
+        data.x = F.log_softmax(data.x.view(1, 2), dim=1)
+        log.debug(data.x.size())
+        return data.x
 
 class GCN_32(torch.nn.Module):
 
--- network/utils.py
+++ network/utils.py
@@ -1,6 +1,7 @@
 import network.gcn_network
 
 networks = {
+                'GCN_test' : network.gcn_network.GCN_test,
                 'GCN_32' : network.gcn_network.GCN_32,
                 'GCN_32_64' : network.gcn_network.GCN_32_64,
                 'GCN_32_64_128' : network.gcn_network.GCN_32_64_128
