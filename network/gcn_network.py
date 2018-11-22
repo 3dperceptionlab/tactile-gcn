14b36ebf9dd24ac4fce609a8135f2a9c68603a9a
Albert Garcia <agarcia@dtic.ua.es>
2018-11-22 17:33:12 +0100

    add two GCN networks with one and three layers #10
    We have added a couple more models to the networks module that
    represent another two GCN-based networks this time with one and
    three layers instead of two as we did with the baseline. Their
    filters are 32 and 32/64/128 features respectively. The goal
    was to experiment with an increasing number of layers.

--- network/gcn_network.py
+++ network/gcn_network.py
@@ -15,6 +15,28 @@ def normalized_cut_2d(edge_index, pos):
     edge_attr = torch.norm(pos[row] - pos[col], p=2, dim=1)
     return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
 
+class GCN_32(torch.nn.Module):
+
+    def __init__(self, numFeatures, numClasses):
+
+        super().__init__()
+
+        self.conv1 = GCNConv(numFeatures, 32)
+        self.fc1 = torch.nn.Linear(32, numClasses)
+
+    def forward(self, data):
+        data.x = F.relu(self.conv1(data.x, data.edge_index))
+        data.x = F.dropout(data.x, training=self.training)
+
+        weight = normalized_cut_2d(data.edge_index, data.pos)
+        cluster = graclus(data.edge_index, weight, data.x.size(0))
+        data.x, batch = max_pool_x(cluster, data.x, data.batch)
+        data.x = global_mean_pool(data.x, batch)
+
+        data.x = self.fc1(data.x)
+        data.x = F.log_softmax(data.x, dim=1)
+        return data.x
+
 class GCN_32_64(torch.nn.Module):
 
     def __init__(self, numFeatures, numClasses):
@@ -29,6 +51,32 @@ class GCN_32_64(torch.nn.Module):
         data.x = F.relu(self.conv1(data.x, data.edge_index))
         data.x = F.dropout(data.x, training=self.training)
         data.x = self.conv2(data.x, data.edge_index)
+
+        weight = normalized_cut_2d(data.edge_index, data.pos)
+        cluster = graclus(data.edge_index, weight, data.x.size(0))
+        data.x, batch = max_pool_x(cluster, data.x, data.batch)
+        data.x = global_mean_pool(data.x, batch)
+
+        data.x = self.fc1(data.x)
+        data.x = F.log_softmax(data.x, dim=1)
+        return data.x
+
+class GCN_32_64_128(torch.nn.Module):
+
+    def __init__(self, numFeatures, numClasses):
+
+        super().__init__()
+
+        self.conv1 = GCNConv(numFeatures, 32)
+        self.conv2 = GCNConv(32, 64)
+        self.conv3 = GCNConv(64, 128)
+        self.fc1 = torch.nn.Linear(128, numClasses)
+
+    def forward(self, data):
+        data.x = F.relu(self.conv1(data.x, data.edge_index))
+        data.x = F.dropout(data.x, training=self.training)
+        data.x = self.conv2(data.x, data.edge_index)
+        data.x = self.conv3(data.x, data.edge_index)
 
         weight = normalized_cut_2d(data.edge_index, data.pos)
         cluster = graclus(data.edge_index, weight, data.x.size(0))
--- network/utils.py
+++ network/utils.py
@@ -1,6 +1,10 @@
 import network.gcn_network
 
-networks = { 'GCN_32_64' : network.gcn_network.GCN_32_64 }
+networks = {
+                'GCN_32' : network.gcn_network.GCN_32,
+                'GCN_32_64' : network.gcn_network.GCN_32_64,
+                'GCN_32_64_128' : network.gcn_network.GCN_32_64_128
+}
 
 def get_network(name, numFeatures, numClasses):  
   return networks[name](numFeatures, numClasses)
